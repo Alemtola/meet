02fb1cbf33989fef7e9a5c1cc138d736da684471
Alemayehu <88435881+Alemtola@users.noreply.github.com>
2021-11-16 19:18:46 +0100

    updated app structure for event list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import EventList from './EventList';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <EventList />
+      </div>
+    );
+  }
 }
+
 
 export default App;
