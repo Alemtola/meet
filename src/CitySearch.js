69cb2f4a2bd6ca43a1802ad43e27d75659ab2529
Alemayehu <88435881+Alemtola@users.noreply.github.com>
2021-11-17 13:56:31 +0100

    updated for event handler

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -17,6 +17,12 @@ class CitySearch extends Component {
       suggestions,
     });
   }
+
+  handleItemClicked = (suggestion) => {
+    this.setState({
+      query: suggestion
+    });
+  }
   
   render() {
     return (
@@ -31,7 +37,7 @@ class CitySearch extends Component {
         <ul className="suggestions">
 
           {this.state.suggestions.map((suggestion) => (
-            <li key={suggestion}>{suggestion}</li>
+            <li key={suggestion} onClick={() => this.handleItemClicked(suggestion)}>{suggestion}</li>
           ))}
 
           <li key='all'>
