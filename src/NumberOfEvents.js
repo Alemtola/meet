b0c1b164045d0064263a8dd5efdf749a2f42bc0b
Alemayehu <88435881+Alemtola@users.noreply.github.com>
2021-11-18 21:13:57 +0100

    update

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -2,21 +2,6 @@ import React, {Component} from 'react';
 
 class NumberOfEvents extends Component {
 
- /* state = {
-    numberOfEvents: 32
-  }
-
-  handleInputChanged = (event) => {
-    const number = event.target.value;
-    if (number <= 0 || number > 32) {
-      return window.alert('Select between 1-32');
-    } else {
-      this.setState({ 
-        numberOfEvents: number,
-      });
-    }
-  }; */
-
   render() {
     return (
       <div className="NumberOfEvents">
