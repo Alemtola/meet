7173a6734613866288246296e035e021cede9ac5
Alemayehu <88435881+Alemtola@users.noreply.github.com>
2021-11-25 17:53:04 +0100

    updatd welcome screen

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -1,46 +1,51 @@
 import React from "react";
 import './WelcomeScreen.css';
 import logo from './images/logo.png';
+import Container from 'react-bootstrap/Container';
 
 function WelcomeScreen(props) {
   return props.showWelcomeScreen ?
     (
-    <div className="WelcomeScreen">
-     
-        <div className="App-logo" >
-          <img src={logo} alt="meet app logo"/>
-        </div>
-        <h1>Welcome to the Meet app</h1>
-        <h4>
-        Log in to see upcoming events around the world for
-        Web Developers
-        </h4>
-        <div className="button_cont" align="center">
-          <div className="google-btn">
-              <div className="google-icon-wrapper">
-                <img
-                class="google-icon"
-                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log
-                o.svg"
-                alt="Google sign-in"
-                />
+      <Container >
+        <div className="WelcomeScreen-wrapper">
+          <div className="WelcomeScreen">
+          
+              <div className="App-logo" >
+                <img src={logo} alt="meet app logo"/>
               </div>
-              <button onClick={() => { props.getAccessToken() }}
+              <h1>Welcome to the Meet app</h1>
+              <br/>
+              <h5>
+              Log in to see upcoming events around the world for
+              Web Developers
+              </h5> 
+              <div className="button_cont" align="center">
+                <div className="google-btn">
+                    <div className="google-icon-wrapper">
+                      <img
+                      class="google-icon"
+                      src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                      alt="Google sign-in"
+                      />
+                    </div>
+                    <button onClick={() => { props.getAccessToken() }}
+                      rel="nofollow noopener"
+                      class="btn-text"
+                      >
+                      <b>Sign in with google</b>
+                    </button>
+                </div>
+              </div>
+              <a
+                href="https://alemtola.github.io/meet/privacy.html"
                 rel="nofollow noopener"
-                class="btn-text"
                 >
-                <b>Sign in with google</b>
-              </button>
+                Privacy policy
+              </a>
+            
           </div>
         </div>
-        <a
-          href="https://alemtola.github.io/meet/privacy.html"
-          rel="nofollow noopener"
-          >
-          Privacy policy
-        </a>
-      
-    </div>
+    </Container>
   )
 : null
 }
